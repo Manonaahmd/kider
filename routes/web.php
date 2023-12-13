f887241dcdbd8bbdb1c75ec01d558607f25706b0
Mona Mohsen Ahmed <129124836+Manonaahmd@users.noreply.github.com>
2023-12-13 13:40:51 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KidController;
-
+use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,6 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('blog')->group(function () {
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
+});
+
 Route::get('kider',[KidController::class, 'index' ]);
 
 Route::get('page',[KidController::class, 'create' ]);
+Route::get('/', KidController::class);
+Route::fallback(KidController::class);
